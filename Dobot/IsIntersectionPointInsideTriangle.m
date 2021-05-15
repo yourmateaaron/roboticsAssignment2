3ad80130fa977175cef82b7a7eb7765c2d91b97d
Aaron Phan <11409481@student.uts.edu.au>
2021-05-16 02:26:39 +1000

    added lab5 intersection inside triangle check function

---
+++ Dobot/IsIntersectionPointInsideTriangle.m
@@ -0,0 +1,36 @@
+%% IsIntersectionPointInsideTriangle
+% Given a point which is known to be on the same plane as the triangle,
+% determin if the point is
+% inside (result == 1) or
+% outside a triangle (result == 0)
+
+function result = IsIntersectionPointInsideTriangle(intersectP,triangleVerts)
+
+u = triangleVerts(2,:) - triangleVerts(1,:);
+v = triangleVerts(3,:) - triangleVerts(1,:);
+
+uu = dot(u,u);
+uv = dot(u,v);
+vv = dot(v,v);
+
+w = intersectP - triangleVerts(1,:);
+wu = dot(w,u);
+wv = dot(w,v);
+
+D = uv*uv - uu*vv;
+
+%get and test parametric coordss (s and t)
+s = (uv * wv - vv* wu) / D;
+if (s<0.01 || s>1.0)
+    result = 0;     %intersectP is outside Triangle
+    return;
+end
+
+t = (uv * wu - uu * wv) / D;
+if (t<0.0 || (s+t)>1.0)
+    result = 0;     %intersectP is outside Triangle
+    return;
+end
+
+result = 1;         %interesectP is inside Triangle
+end
